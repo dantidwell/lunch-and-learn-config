af43eba71fe269447ab44a8b16b95168d8a13857
Ubuntu <dtidwell@dt-goci-srv.dt-goci-srv.b7.internal.cloudapp.net>
2015-05-27 17:12:49 +0000

    again..

--- provision_at.sh
+++ provision_at.sh
@@ -9,4 +9,4 @@ AZURE_PASSWORD="myPassword123"
 AZURE_REGION="East US"
 TCP_ENDPOINTS="80"
 
-knife azure server create  --config /etc/chef/knife.rb --azure-dns-name $AZURE_DNS_NAME --azure-vm-name $AZURE_VM_NAME --azure-vm-size $AZURE_VM_SIZE --azure-source-image $AZURE_SOURCE_IMAGE --ssh-user $AZURE_USER --ssh-password $AZURE_PASSWORD --azure-service-location $AZURE_REGION
+knife azure server create --config /etc/chef/knife.rb --azure-dns-name $AZURE_DNS_NAME --azure-vm-name $AZURE_VM_NAME --azure-vm-size $AZURE_VM_SIZE --azure-source-image $AZURE_SOURCE_IMAGE --ssh-user $AZURE_USER --ssh-password $AZURE_PASSWORD --azure-service-location "$AZURE_REGION"
